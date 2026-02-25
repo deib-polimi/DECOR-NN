10c967f1fd90738fa634052695c7e19be3c69bc4
davideciamacco <davide.ciamacco@mail.polimi.it>
2026-02-25 18:11:23 +0100

    restore old version of update

--- cpu/controller.py
+++ cpu/controller.py
@@ -44,35 +44,32 @@ def next_allocation(progress, total_progress, set_point, job):
 
     return cs, csp
 
-def update(desired, job, last_used_core, scaling_factor):
-
-    #actual_cores = desired * scaling_factor
+def update(desired, job, scaling_factor):
+    actual_cores = desired * scaling_factor
     # Round the final allocation
-    #job.csi_old = actual_cores - job.csp
+    job.csi_old = actual_cores - job.csp
     
-    #quantized_cores = round(actual_cores , QUANTUM_DIGITS)
-    #final_cores = math.ceil(max(MIN_CORES, desired)) #critical when having N jobs where N is higher than the number of cores
+    quantized_cores = round(actual_cores , QUANTUM_DIGITS)
+    final_cores = max(MIN_CORES, quantized_cores) #critical when having N jobs where N is higher than the number of cores
+    #final_cores = math.ceil(max(MIN_CORES, quantized_cores))
 
-    final_cores = max(MIN_CORES, desired)
+    cpu_quota = int(final_cores * CPU_PERIOD)
 
     #cpu_quota = int(final_cores * CPU_PERIOD)
     if final_cores != job.current_cores:
         job.csi_old = final_cores - job.csp
         try: 
-            subprocess.run(f'docker update --cpuset-cpus="{last_used_core}-{last_used_core + final_cores - 1}" {job.container_name}',
+            subprocess.run(f'docker update --cpu-quota="{cpu_quota}" {job.container_name}',
                 shell=True, check=True, capture_output=True)
-            print(f"[{job.id}] From {last_used_core} to {last_used_core + final_cores - 1}")
+            print(f"[{job.id}] Updated cores to: {final_cores}")
             alloc_time = time.monotonic() - job.start_time
             with open(job.allocations_file, "a") as f:
                 f.write(f"{alloc_time},{job.current_cores}\n")
                 f.write(f"{alloc_time},{final_cores}\n")
             
             job.current_cores = final_cores
-            return last_used_core + final_cores
         except subprocess.CalledProcessError as e:
             print(f"[{job.id}] Error updating CPU quota: {e.stderr.decode()}")
-    else:
-        return last_used_core + final_cores
 
 def read_progress(job):
     """Reads the progress from the job's progress file."""
@@ -142,13 +139,12 @@ def schedule(shell):
             if total_desired_cores > MAX_CORES:
                 scaling_factor = MAX_CORES / total_desired_cores
 
-            last_used_core = STARTING_CORE
             # 3. Apply the new allocations
             for job in shell.jobs:
                 if job.id not in desired_allocations:
                     continue
                 else:
-                    last_used_core = update(desired_allocations[job.id], job, last_used_core, scaling_factor)
+                    update(desired_allocations[job.id], job, scaling_factor)
 
 def start(model: str, num_batches: int, batch_size: int, epochs: int, 
           container_name: str, progress_file_path: str, image_name: str):
